5b9cc50bf2ac7c86e1609d62e3336a79cd55fd04
史 翔新 <x_shi@yumemi.co.jp>
2021-02-20 17:09:28 +0900

    Initial Xcode project

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 史 翔新 on 2021/02/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/ViDRep_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViDRep_SampleApp.swift
+//  Shared
+//
+//  Created by 史 翔新 on 2021/02/20.
+//
+
+import SwiftUI
+
+@main
+struct ViDRep_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
