e2bf114ff19ace8c452b46dc63dfd61c5cde6d27
史 翔新 <x_shi@yumemi.co.jp>
2021-05-26 01:03:47 +0900

    Add an error type to represent save load failure

--- Shared/Source/Models/Data/GeneralError.swift
+++ Shared/Source/Models/Data/GeneralError.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 enum GeneralError: Error {
+    case dataSaveLoadError(DataSaveLoadError)
     case urlError(URLError)
     case decodingError(DecodingError)
     case unknownError(Error)
@@ -17,6 +18,9 @@ extension GeneralError {
     
     var message: String {
         switch self {
+        case .dataSaveLoadError(let error):
+            return "Failed with data save load error: \(error)"
+        
         case .urlError(let error):
             return "Failed with URL error: \(error)"
             
@@ -35,6 +39,14 @@ extension GeneralError: Identifiable {
     
     var id: String {
         return message
+    }
+    
+}
+
+extension GeneralError: Equatable {
+    
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        return lhs.id == rhs.id
     }
     
 }
