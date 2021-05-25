2564bc4182e862377ceca3940264751905519e66
史 翔新 <x_shi@yumemi.co.jp>
2021-05-26 01:25:57 +0900

    Add AppResolver

---
+++ Shared/Source/App/AppResolver.swift
@@ -0,0 +1,60 @@
+//
+//  AppResolver.swift
+//  ViDRep-Sample
+//
+//  Created by 史 翔新 on 2021/05/25.
+//
+
+import SwiftUI
+import Combine
+
+final class AppResolver: ObservableObject {
+    
+    private lazy var repository = AppRepository()
+    
+    private lazy var userDefaultDatabase = UserDefaultsDatabase()
+    private lazy var apiClient = CataasAPIClient()
+    
+    private lazy var catImageSearchTextReducer = CatImageSearchTextReducer(maxCharacterCount: 10)
+    
+    private lazy var imageSearchingDispatcher = ImageSearchingDispatcher(reducer: catImageSearchTextReducer,
+                                                                         apiClient: apiClient,
+                                                                         repository: repository)
+    private lazy var imageSavingDispatcher = ImageSavingDispatcher(userDatabase: userDefaultDatabase,
+                                                                   repository: repository)
+    
+    private lazy var appRouter = AppRouter(resolverDelegate: self,
+                                           repository: repository)
+        
+    var objectWillChange: ObservableObjectPublisher {
+        repository.objectWillChange
+    }
+    
+}
+
+extension AppResolver {
+    
+    @ViewBuilder
+    func makeView() -> some View {
+        appRouter.makeAppView()
+    }
+    
+}
+
+extension AppResolver: AppRouterResolverDelegate {
+    
+    @ViewBuilder
+    func appRouterNeedsView(for viewID: ViewID) -> some View {
+        switch viewID {
+        case .searchScene:
+            SearchScene(routerDelegate: appRouter, dispatcher: imageSearchingDispatcher, repository: repository)
+            
+        case .savedImageListScene:
+            SavedImageListScene(routerDelegate: appRouter, dispatcher: imageSavingDispatcher, repository: repository)
+            
+        case .imagePreviewScene(image: let image):
+            ImagePreviewScene(dispatcher: imageSavingDispatcher, repository: repository, image: image)
+        }
+    }
+    
+}
